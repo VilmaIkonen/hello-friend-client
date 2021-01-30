517387812b2ed07006df8997c72338043b87e3ea
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-01-30 20:54:28 +0200

    Required fields and helper text for tags field added

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -21,6 +21,7 @@ const Form = ({currentId, setCurrentId}) => {
 
   // Fetching data for the update of a post and Populate the values of the Form
   const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
+
   useEffect(() => {
     if(post) setPostData(post);
   }, [post])
@@ -49,67 +50,74 @@ const Form = ({currentId, setCurrentId}) => {
     })
   }
 
+  const errorTags = postData.tags[0]==='#';
+
   return (
     <Paper className={classes.paper}>
     {/* classes.root/classes.form --> Multiple templates w template string to get styling from MUI to both root and form (see styles.css) */}
-      <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}> 
-        <Typography className={classes.heading}variant="h6">{currentId ? 'Edit the' : 'Create a'} note</Typography>
+      <form autoComplete='off' className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}> 
+        <Typography className={classes.heading}variant='h6'>{currentId ? 'Edit the' : 'Create a'} note</Typography>
         <TextField 
-          name="creator" 
-          variant="outlined" 
-          label="Creator" 
+          required
+          name='creator' 
+          variant='outlined' 
+          label='Creator' 
           fullWidth 
           value={postData.creator} 
           onChange={(e) => setPostData({...postData, creator: e.target.value})} 
         /> 
-          {/* "spread + creator: Keep other data, change only a specific property of text field" */}       
+          {/* 'spread + creator: Keep other data, change only a specific property of text field' */}       
         <TextField 
-          name="title" 
-          variant="outlined" 
-          label="Title" 
+          required
+          name='title' 
+          variant='outlined' 
+          label='Title' 
           fullWidth 
           value={postData.title} 
           onChange={(e) => setPostData({...postData, title: e.target.value})} 
         />
         <TextField 
-          name="message" 
-          variant="outlined" 
-          label="Message" 
+          required
+          name='message' 
+          variant='outlined' 
+          label='Message' 
           fullWidth 
+          multiline={true}
           value={postData.message} 
           onChange={(e) => setPostData({...postData, message: e.target.value})} 
         />
         <TextField 
-          name="tags" 
-          variant="outlined" 
-          label="Tags" 
+          name='tags' 
+          variant='outlined' 
+          label='Tags' 
           fullWidth 
           value={postData.tags} 
           onChange={(e) => setPostData({...postData, tags: e.target.value.split(',')})}
+          helperText={errorTags ? 'Type tags w/o #, separated by comma' : ''}
+          error={errorTags}          
         />
         <div className={classes.fileInput}>
           <FileBase
-            type="file"
+            type='file'
             multiple={false}
             onDone={({base64}) => setPostData({...postData, selectedFile: base64})}
           />        
         </div>
         <Button 
           className={classes.buttonSubmit} 
-          variant="contained"
-          size="large"
-          type="submit"
+          variant='contained'
+          size='large'
+          type='submit'
           fullWidth>Submit
         </Button>
         <Button 
           className={classes.buttonClear}
-          variant="contained" 
-          size="small"
+          variant='contained' 
+          size='small'
           onClick={clear}
           fullWidth>Clear
         </Button>
       </form>
-
     </Paper>
   )
 }
